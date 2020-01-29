1aabf919e0e8c6c29353b0deb4e71caeec991374
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-29 04:55:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+<<<<<<< HEAD
 
 nds = directors_database
 def gross_for_director(dh)   #dh stands for director hash
@@ -22,12 +23,44 @@ return result
 end
 # { directorOne => allTheMoneyTheyMade, ... }
 a = gross_for_director(nds[1])
+=======
+
+
+def gross_for_director(director_data)
+# Write a method that, given an NDS creates a new Hash
+# The return value should be like:
+#nds = directors_database
+nds = director_data
+result={}
+nod = nds.length #number of directors_database
+i=0# Director index
+while i<nod do
+  nom = nds[i][:movies].length
+  j=0#Movie index
+  t=0#total of all the movies of one director
+  while j<nom do
+    t=t+nds[i][:movies][j][:worldwide_gross]
+    j=j+1
+  end
+  result [nds[i][:name]] = t
+  i=i+1
+end
+
+return result
+end
+# { directorOne => allTheMoneyTheyMade, ... }
+a = gross_for_director(directors_database)
+>>>>>>> bf113b5fff1ca2624c28a84f0f2a1a63e08850aa
 puts a
 
 
 def directors_totals(nds)
+<<<<<<< HEAD
   dtotals=[]
   names=[]
+=======
+
+>>>>>>> bf113b5fff1ca2624c28a84f0f2a1a63e08850aa
   result = {}
   i=0
   while i<nds.length do
--- lib/test.rb
+++ lib/test.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 nds = directors_database
+<<<<<<< HEAD
 def gross_for_director(dh)   #dh stands for director hash
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
@@ -37,6 +38,22 @@ end
 a= directors_totals(nds)
 puts a
 
+=======
+dname="Spike Lee"
+i=0
+while i < nds.length do
+  if nds[i][:name]== dname
+    puts 'gotem'
+    i=1+1
+  else
+    puts 'aintgotem'
+    puts i
+    puts dname
+    puts nds[i][:name]
+    i=i+1
+  end
+end
+>>>>>>> bf113b5fff1ca2624c28a84f0f2a1a63e08850aa
 
 
 
